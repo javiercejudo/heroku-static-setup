41b19442eac8243d596f6164d711933902a4add2
Javier Cejudo <javier@javiercejudo.com>
2015-02-01 02:52:37 +1100

    Consistent quotes in index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ var express = require('express'),
     app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.set('path', (process.env.PUBLIC_PATH || "public"));
+app.set('path', (process.env.PUBLIC_PATH || 'public'));
 
 app.use(compression());
 app.use(express.static(__dirname + '/' + app.get('path')));
 
 app.listen(app.get('port'), function() {
-  console.log("Listening on port " + app.get('port'));
+  console.log('Listening on port ' + app.get('port'));
 });
